2d8854a797b41b1c32a433b00c6340f05a59dd1f
rana <ranahamdy269@gmail.com>
2023-08-30 23:23:32 +0300

    info @ appointment screen for patient

--- lib/modules/ui/doctor_screens/doctor_schedule_&_patientinfo_screens/docter_patientinfo_screen.dart
+++ lib/modules/ui/doctor_screens/doctor_schedule_&_patientinfo_screens/docter_patientinfo_screen.dart
@@ -1,3 +1,4 @@
+import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -10,22 +11,63 @@ class Doctor_PatientInfo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(
-        backPage: 'DoctorScheduleScreen',
-        pageTitle: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              "Patient Info",
-              style: TextStyle(
-                  color: MyColors.black,
-                  fontFamily: 'Inter',
-                  fontWeight: FontWeight.w600,
-                  fontSize: 18.sp),
-            )
+        appBar: MyAppBar(
+          backPage: 'DoctorListForPatientScreen',
+          pageTitle: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Patient Info",
+                style: TextStyle(
+                    color: MyColors.black,
+                    fontFamily: 'Inter',
+                    fontWeight: FontWeight.w600,
+                    fontSize: 18.sp),
+              )
+            ],
+          ),
+          actionIcon: [
+            Padding(
+                padding: EdgeInsets.only(right: 25.sp),
+                child: Image.asset("assets/images/doctor.png"))
           ],
         ),
-      ),
-    );
+        body: ConditionalBuilder(
+          condition: true,
+          builder: (context) => ListView.separated(
+            physics: const BouncingScrollPhysics(),
+            itemCount: 7,
+            itemBuilder: (context, index) => Padding(
+              padding: EdgeInsets.all(15.sp),
+              child: const Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "About : ",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                      Text("Ahmed "),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            separatorBuilder: (BuildContext context, int index) =>
+                SizedBox(height: 1.h),
+          ),
+          fallback: (context) => Center(child: CircularProgressIndicator()),
+        ),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.transparent,
+          elevation: 0.sp,
+          child: Padding(
+            padding: EdgeInsets.all(25.sp),
+            child:
+                const MyBlueButton(text: "Back", page: 'DoctorScheduleScreen'),
+          ),
+        ));
   }
 }

54301a6ea7da6ddb9980c4949ac2e55460f5a957
rana <ranahamdy269@gmail.com>
2023-08-30 13:47:06 +0300

    adjustment

--- lib/modules/ui/doctor_screens/doctor_schedule_&_patientinfo_screens/docter_patientinfo_screen.dart
+++ lib/modules/ui/doctor_screens/doctor_schedule_&_patientinfo_screens/docter_patientinfo_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:nutri_mind_application/shared/Constants/colors.dart';
+import 'package:nutri_mind_application/shared/widgets/screens_widgets.dart';
 
 class Doctor_PatientInfo extends StatelessWidget {
   const Doctor_PatientInfo({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: MyAppBar(
+        backPage: 'DoctorScheduleScreen',
+        pageTitle: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Patient Info",
+              style: TextStyle(
+                  color: MyColors.black,
+                  fontFamily: 'Inter',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 18.sp),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }

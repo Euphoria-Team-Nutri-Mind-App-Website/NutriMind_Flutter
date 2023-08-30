d69b74032ad870e5fb8e0dd9d8c84340e3bf484c
rana <ranahamdy269@gmail.com>
2023-08-30 04:24:28 +0300

    doctor register screen

--- lib/modules/ui/doctors_screen/doctor_auth_screen/registeration_doctors.dart
+++ lib/modules/ui/doctors_screen/doctor_auth_screen/registeration_doctors.dart
@@ -1,1 +1,125 @@
 
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../shared/Constants/colors.dart';
+import '../../../../shared/widgets/screens_widgets.dart';
+
+class RegistrationDoctorScreen extends StatelessWidget {
+  const RegistrationDoctorScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pushNamed(context, 'ContinueScreen');
+            },
+            icon: const Row(
+              children: [
+                Icon(
+                  Icons.arrow_back_ios,
+                  color: MyColors.black,
+                  size: 25,
+                ),
+              ],
+            )),
+        title: Image(
+          image: const AssetImage(
+            "assets/images/logo.png",
+          ),
+          width: 80.w,
+        ),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.all(11.0),
+            child: Icon(Icons.menu, color: MyColors.black),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(25.sp),
+            color: MyColors.white,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Welcome to Nutri Mind",
+                  style: TextStyle(
+                      fontFamily: 'Inter',
+                      fontWeight: FontWeight.w400,
+                      fontSize: 13.sp,
+                      color: MyColors.grey),
+                ),
+                SizedBox(
+                  height: 40.h,
+                ),
+                const MyTextField(
+                  hintText: 'Enter your name',
+                  obscureText: false,
+                  textType: TextInputType.name,
+                ),
+                SizedBox(
+                  height: 40.h,
+                ),
+                const MyTextField(
+                  hintText: 'Email',
+                  obscureText: false,
+                  textType: TextInputType.emailAddress,
+                ),
+                SizedBox(
+                  height: 40.h,
+                ),
+                const MyTextField(
+                  hintText: 'Phone Number',
+                  obscureText: false,
+                  textType: TextInputType.phone,
+                ),
+                SizedBox(
+                  height: 40.h,
+                ),
+                const MyTextField(
+                  hintText: 'Address',
+                  obscureText: false,
+                  textType: TextInputType.name,
+                ),
+                SizedBox(
+                  height: 40.h,
+                ),
+                const MyTextField(
+                  hintText: 'National Id',
+                  obscureText: false,
+                  textType: TextInputType.phone,
+                ),
+                SizedBox(
+                  height: 40.h,
+                ),
+                const MyTextField(
+                  hintText: 'Gender',
+                  obscureText: false,
+                  textType: TextInputType.text,
+                ),
+                SizedBox(
+                  height: 40.h,
+                ),
+                const MyTextField(
+                  hintText: 'Qualivication',
+                  obscureText: false,
+                  textType: TextInputType.text,
+                ),
+                SizedBox(
+                  height: 28.h,
+                ),
+                const MyBlueButton(text: "Register", page: ''),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}

47458d81c14b06c9c4327cae32a2e023d6445e23
Habeba-Elhosary <habebaelhosary98@gmail.com>
2023-09-04 13:36:36 +0300

    Updates on Set Tall Screens

--- lib/modules/ui/Patient_screens/patient_first_set_tall_weight_screens/patient_set_tall.dart
+++ lib/modules/ui/Patient_screens/patient_first_set_tall_weight_screens/patient_set_tall.dart
@@ -37,7 +37,7 @@ class _PatientSetTallScreenState extends State<PatientSetTallScreen> {
             padding: EdgeInsets.only(left: 25.sp, right: 25.sp, top: 40.sp),
             color: MyColors.white,
             child: Column(
-              crossAxisAlignment:CrossAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
                   'Please Enter you tall !',
@@ -47,7 +47,166 @@ class _PatientSetTallScreenState extends State<PatientSetTallScreen> {
                       fontFamily: 'Inter',
                       fontSize: 15.sp),
                 ),
-                const MyBlueButton(text: 'Next',page: 'PatientSetWeightScreen',)
+                SizedBox(
+                  height: 60.h,
+                ),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Column(
+                    children: [
+                      Text(
+                        '7.0 -',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Text(
+                        '6.0 -',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Text(
+                        '5.0 -',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Text(
+                        '4.0 -',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 20.w),
+                  Stack(
+                    children: [
+                      Container(
+                        width: 80.w,
+                        height: 180.h,
+                        decoration: BoxDecoration(
+                          color: MyColors.babyBlue,
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 120.sp),
+                        width: 80.w,
+                        height: 70.h,
+                        decoration: BoxDecoration(
+                          color: MyColors.darkBlue,
+                          borderRadius: BorderRadius.only(
+                              bottomRight: Radius.circular(10.r),
+                              bottomLeft: Radius.circular(10.r)),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: 20.w),
+                  Column(
+                    children: [
+                      Text(
+                        '- 220',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Text(
+                        '- 180',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Text(
+                        '- 150',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                      SizedBox(
+                        height: 30.h,
+                      ),
+                      Text(
+                        '- 120',
+                        style: TextStyle(
+                            color: MyColors.grey,
+                            fontWeight: FontWeight.w600,
+                            fontFamily: 'Inter',
+                            fontSize: 15.sp),
+                      ),
+                    ],
+                  ),
+                ]),
+                SizedBox(
+                  height: 60.h,
+                ),
+                Center(
+                  child: Container(
+                    width: 90.w,
+                    padding: EdgeInsets.all(15.sp),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10.r),
+                      border: Border.all(color: MyColors.darkBlue),
+                      boxShadow: const [
+                        BoxShadow(
+                          color: MyColors.darkBlue,
+                          blurRadius: 5.0,
+                        ), //BoxShadow
+                        BoxShadow(
+                          color: MyColors.white,
+                          offset: Offset(0.0, 0.0),
+                          blurRadius: 0.0,
+                          spreadRadius: 0.0,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: Text(
+                        "160cm",
+                        style: TextStyle(
+                            fontSize: 12.sp,
+                            color: MyColors.black,
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'Inter'),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.h),
+                const MyBlueButton(
+                  text: 'Next',
+                  page: 'PatientSetWeightScreen',
+                )
               ],
             ),
           ),

9606c83ffa209b8fa435f5bd2a143bdd96707683
Habeba-Elhosary <habebaelhosary98@gmail.com>
2023-09-09 14:11:19 +0300

    logic sign up

--- lib/modules/blocs/auth_cubit/auth_state.dart
+++ lib/modules/blocs/auth_cubit/auth_state.dart
@@ -1,23 +1,18 @@
 part of 'auth_cubit.dart';
 
-@immutable
 abstract class AuthState {}
 
 class AuthInitialState extends AuthState {}
-
+//***************************************************************************************************************
 class RegisterLoadingState extends AuthState {}
-
 class RegisterSuccessState extends AuthState {}
-
 class RegisterFailedState extends AuthState {
   String message;
   RegisterFailedState({required this.message});
 }
-
+//***************************************************************************************************************
 class LoginLoadingState extends AuthState {}
-
 class LoginSuccessState extends AuthState {}
-
 class LoginFailedState extends AuthState {
   String message;
   LoginFailedState({required this.message});
